10b122dec511a8113fd9428eef46962a12e6b91a
Pratik Mandge <88229260+pratikmandge@users.noreply.github.com>
2025-09-14 09:40:11 +0530

    Delete Frontend directory

--- Frontend/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-900 text-white py-6 text-center" role="contentinfo">
-      <p>&copy; {new Date().getFullYear()} GlobalWebix. All Rights Reserved.</p>
-    </footer>
-  )
-}
-
-
